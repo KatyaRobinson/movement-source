1063b179d79b31b0725704597a7f247766eb6929
katya <katya.gurina@yahoo.com>
2017-01-16 15:06:44 -0500

    service page main conteiner resizing version one completed

--- js/book.js
+++ js/book.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
+
+          var $windowWidth = $(window).width();
+          var wrapper = $("#wrapper");
+
+          // hide  nav logo which only appears when nav gets stuck to top on scrolling down
           $("#nav-logo").hide();
+
+
+
         //function to display appointment times
           var currentDate = '';
           var dayName = '';
@@ -65,5 +73,64 @@ $(document).ready(function(){
           $("#nav-logo").hide('fast');
         }
   });
+
+        // change padding of wrapper on load depending on the window width
+          $(function(){
+            if ($windowWidth < 1713 && $windowWidth > 761){
+              console.log("padding added");
+              wrapper.css("padding-left", 250);
+            }
+
+           else if ($windowWidth > 1713 || $windowWidth < 761){
+              console.log("padding removed");
+              wrapper.css("padding-left", 0);
+            }
+
+          });
+
+    // move container on page resize to accomodate for sidevar menu
+          $(window).resize(function(){
+            console.log($windowWidth);
+            if ($windowWidth < 1670 && $windowWidth > 761){
+              console.log("padding added");
+              wrapper.css("padding-left", 250);
+            }
+
+            else if ($windowWidth > 1670 || $windowWidth < 761){
+              console.log("padding removed");
+              wrapper.css("padding-left", 0);
+            }
+
+          });
+
+
+
+   //function for smooth scrolling on Services page       
+          $(function() {
+              $('a[href*="#"]:not([href="#"])').click(function() {
+                if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                  var target = $(this.hash);
+                  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                  if (target.length) {
+                    $('html, body').animate({
+                      scrollTop: target.offset().top - 100
+                    }, 1000);
+                    return false;
+                  }
+                }
+              });
+});
+
+  // Scrolls down to the calendar when button is pressed on Services page
+          $(function(){
+
+                // when the Secure My Appt button is clicked 
+                $("#groupClassRequest").click(function(){
+                $("body, html").animate({ 
+                     scrollTop: $('#calendar').offset().top 
+                     }, 600);
+            });
+
+            }) 
           
    })    
