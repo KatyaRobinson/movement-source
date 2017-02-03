493d4da385c370fd56e609dc7b8fb211295b3aa4
katya <katya.gurina@yahoo.com>
2017-02-03 11:58:39 -0500

    form validation completed, started log in page

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+$servername = "localhost";
+$username = "katya";
+$password = "p@ssw0rd";
+$database = "movement-source";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
--- js/form-validator.js
+++ js/form-validator.js
@@ -1,24 +1,33 @@
 
-$('#cust-contact').validate({
+// Wait for the DOM to be ready
+$(function() {
+  // Initialize form validation on the registration form.
+  // It has the name attribute "registration"
+  $("form[id='cust-contact']").validate({
+    // Specify validation rules
     rules: {
-        name: {
-            minlength: 2,
-            required: true
-        },
-        email: {
-            required: true,
-            email: true
-        },
-        message: {
-            minlength: 2,
-            required: false
-        }
+      // The key name on the left side is the name attribute
+      // of an input field. Validation rules are defined
+      // on the right side
+      name: "required",
+      phone: "required",
+      email: {
+        required: true,
+        // Specify that email should be validated
+        // by the built-in "email" rule
+        email: true
+      }
     },
-    highlight: function (element) {
-        $(element).closest('.control-group').removeClass('success').addClass('error');
+    // Specify validation error messages
+    messages: {
+      name: "Please enter your firstname",
+      phone: "Please enter your phone number",
+      email: "Please enter a valid email address"
     },
-    success: function (element) {
-        element.text('OK!').addClass('valid')
-            .closest('.control-group').removeClass('error').addClass('success');
+    // Make sure the form is submitted to the destination defined
+    // in the "action" attribute of the form when valid
+    submitHandler: function(form) {
+      form.submit();
     }
+  });
 });
